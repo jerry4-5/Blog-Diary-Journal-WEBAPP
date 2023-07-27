8b167983a555e17335a3bf5adc1d17019cb801a2
jerry4-5 <88873062+jerry4-5@users.noreply.github.com>
2023-07-27 13:07:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ passport.deserializeUser(function(id, done){
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "https://drab-chameleon.cyclic.app/auth/google/secrets",
+  callbackURL: "http://localhost:3000/auth/google/secrets",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb){
